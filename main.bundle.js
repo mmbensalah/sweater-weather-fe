61608303dd3cfdb208169fd8484505bcbd1edcd1
mmbensalah <mmbensalah@gmail.com>
2019-01-25 22:47:05 -0700

    change scss to css. create html div elements for forecast, hourly, daily and div elements w/in those divs for items. next step, figure out how to create new html anchors for each piece of data

--- main.bundle.js
+++ main.bundle.js
@@ -51,99 +51,48 @@
 	    $.ajax({
 	      type: 'GET',
 	      url: 'https://damp-lowlands-57714.herokuapp.com/api/v1/forecast?location=' + $("#search-field").val(),
-	      success: function success() {
-	        var arr = result["data"];
-	        debugger;
-	        // $(".b-current-weather").append(`${arr['data']}`});
+	      success: function success(result) {
+	        currentForecast(result);
+	        dailyForecast(result);
+	        hourlyForecast(result);
 	      }
 	    });
 	  });
 	});
 
-	// var btn = document.getElementById("search-btn");
-	// var favBtn = document.getElementById("fav-btn");
-	// var field = document.getElementById("search-field")
-	// var currentWeather = document.querySelector(".b-current-weather")
-	// var currentWeatherDetails = document.querySelector(".b-current-weather-details")
-	// var currentHourlyDaily = document.querySelector(".b-current-hourly-daily")
-	// btn.addEventListener("click", getWeather);
-	// favBtn.addEventListener("click", getFavorites);
-	//
-	// function getWeather() {
-	//   var ourRequest = new XMLHttpRequest();
-	//   ourRequest.open('GET', `https://damp-lowlands-57714.herokuapp.com/api/v1/forecast?location=${field.value}`)
-	//   ourRequest.onload = function() {
-	//     var jsonData = JSON.parse(ourRequest.responseText)["data"];
-	//     renderHTML(jsonData);
-	//   };
-	//   ourRequest.send();
-	// }
-	//
-	// function getFavorites() {
-	//   var ourRequest = new XMLHttpRequest();
-	//   ourRequest.open('GET', `https://damp-lowlands-57714.herokuapp.com/api/v1/favorites?api_key=sYbUFp5o7OfSCwBEIQHWtA`)
-	//   // ourRequest.setRequestHeader('api_key', 'pKyQaaomPtQVuLqe0GjkRg')
-	//   ourRequest.onload = function() {
-	//     debugger
-	//     var jsonData = JSON.parse(ourRequest.responseText)["data"];
-	//     debugger
-	//     renderHTML(jsonData);
-	//   };
-	//   ourRequest.send();
-	// }
-	//
-	// function renderHTML(jsonData) {
-	//   var jsonArray = Object.values(jsonData)
-	//   var place     = jsonArray[0]
-	//   var forecast  = getValues(jsonArray[2])
-	//   var hourly    = getValues(forecast)[8] //array
-	//   var daily     = getValues(forecast)[9] //array
-	//
-	//   var i;
-	//   for (i = 0; i < hourly.length; i++) {
-	//     var hour = hourly[i];
-	//     hour.temp, hour.time, hour.icon
-	//   }
-	//
-	//   for (i = 0; i < daily.length; i++) {
-	//     var day = daily[i];
-	//     day.time, day.icon, day.rain_prob, day.temp_low, day.temp_high
-	//   }
-	//
-	//   currentWeather.insertAdjacentHTML('beforeend', place);
-	//   currentWeatherDetails.insertAdjacentHTML('beforeend', forecast);
-	// }
-	//
-	// function getValues(data) {
-	//   return Object.values(data);
-	// };
-	//
-	// // function postUser(email, pw) {
-	// //   var ourRequest = new XMLHttpRequest();
-	// //   ourRequest.open('POST', `http://localhost:3000/api/v1/users`)
-	// //   // ourRequest.onload = function() {
-	// //   //   var jsonData = JSON.parse(ourRequest.responseText)["data"];
-	// //   // };
-	// //   ourRequest.send();
-	// //   ourRequest.onreadystatechange = processRequest;
-	// //
-	// //   function processRequest(e) {
-	// //     if (ourRequest.readyState == 4 && ourRequest.status == 200) {
-	// //       alert(ourRequest.responseText);
-	// //     }
-	// //   }
-	// // }
-	//
-	//
-	//
-	// // jsonData.forEach(function(val) {
-	// //   var keys = Object.keys(val);
-	// //   html += "<div class='cat'>";
-	// //   keys.forEach(function(key) {
-	// //     html += "<strong>" + key + "</strong>" + val[key] + "<br>";
-	// //   });
-	// //   html += "</div><br>";
-	// // });
+	function currentForecast(result) {
+	  var current = result["data"];
+	  $(".city").append('' + current["id"]);
+	  $(".desc").append('' + current["attributes"]["forecast"]);
+	  $(".cur-temp").append('' + current["attributes"]["current_temp"]);
+	  $(".cur-time").append('' + current["attributes"]["current_time"]);
+	  $(".feels-like").append('' + current["attributes"]["feels_like"]);
+	  $(".humidity").append('' + current["attributes"]["humidity"]);
+	}
+
+	function dailyForecast(result) {
+	  var dailyData = result["data"]["attributes"]["daily"];
+	  var i;
+	  for (i = 0; i < dailyData.length; i++) {
+	    var day = dailyData[i];
+	    $(".daily").append('' + day["icon"]);
+	    $(".daily").append('' + day["rain_prob"]);
+	    $(".daily").append('' + day["temp_high"]);
+	    $(".daily").append('' + day["temp_low"]);
+	    $(".daily").append('' + day["time"]);
+	  }
+	}
+
+	function hourlyForecast(result) {
+	  var hourlyData = result["data"]["attributes"]["hourly"];
+	  var i;
+	  for (i = 0; i < hourlyData.length; i++) {
+	    var hour = hourlyData[i];
+	    $(".hourly").append('' + hour["icon"]);
+	    $(".hourly").append('' + hour["temp"]);
+	    $(".hourly").append('' + hour["time"]);
+	  }
+	}
 
 /***/ })
 /******/ ]);
